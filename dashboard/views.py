a51f99de162e36ba74449c4b0ad12cba0156b01a
Abdiwak <abdiwakamsalu@gmail.com>
2026-08-25 13:26:31 -0700

    day 6 listing instance, image and network

---
+++ dashboard/views.py
@@ -0,0 +1,80 @@
+import openstack
+
+from django.shortcuts import render
+
+
+def get_openstack_connection():
+    return openstack.connect(cloud="devstack-admin")
+
+
+def home(request):
+    return render(request, "dashboard/home.html")
+
+
+def instances(request):
+    conn = get_openstack_connection()
+
+    servers = conn.compute.servers()
+
+    instance_list = []
+
+    for server in servers:
+
+        addresses = []
+
+        for network_name, network_addresses in server.addresses.items():
+            for address in network_addresses:
+                addresses.append(address.get("addr"))
+
+        # Get flavor information directly from the server
+        flavor_name = "Unknown"
+
+        if server.flavor:
+            flavor_name = server.flavor.get("original_name")
+
+            if not flavor_name:
+                flavor_name = server.flavor.get("id", "Unknown")
+
+        instance_list.append({
+            "id": server.id,
+            "name": server.name,
+            "status": server.status,
+            "addresses": addresses,
+            "flavor": flavor_name,
+        })
+
+    return render(
+        request,
+        "dashboard/instances.html",
+        {
+            "instances": instance_list
+        }
+    )
+
+
+def images(request):
+    conn = get_openstack_connection()
+
+    image_list = conn.image.images()
+
+    return render(
+        request,
+        "dashboard/images.html",
+        {
+            "images": image_list
+        }
+    )
+
+
+def networks(request):
+    conn = get_openstack_connection()
+
+    network_list = conn.network.networks()
+
+    return render(
+        request,
+        "dashboard/networks.html",
+        {
+            "networks": network_list
+        }
+    )
